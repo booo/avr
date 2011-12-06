d40aefb35bc0bac3dea886de5455766f946fa3f4
booo <borgers@mi.fu-berlin.de>
2011-12-06 10:52:40 +0100

    second example

--- 01/main.c
+++ 01/main.c
@@ -1,9 +1,25 @@
 #include <avr/io.h>
 
+#include <stdint.h>
+
 int main(void) {
 
+    //DDRC = 0b00000110;
+    DDRC = 0x06;
+    PORTC = 0xF9;
     while(1) {
         //paste some shit here
+        if (PINC & (1<<PINC0)) {
+            PORTC = 0b11111101;
+            //PORTC = (1<<PC5) | PORTC;
+            //PORTC = (0<<PC4) & PORTC;
+            //grün an - rot aus
+        } else {
+            //PORTC = (1<<PC) | PORTC;
+            //rot an - grün aus
+            PORTC = 0b11111011;
+        }
+        
     }
     return 0;
 }
