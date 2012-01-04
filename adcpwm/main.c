42e7c0937d1193d0f48890fc08bc30bed45289c7
booo <borgers@mi.fu-berlin.de>
2012-01-04 17:15:08 +0100

    change adcpwm example

--- adcpwm/main.c
+++ adcpwm/main.c
@@ -10,55 +10,67 @@
 
 int main(void) {
 
-    DDRD = 0x40;
-    PORTD = 0xBF;
+    DDRD |= (1<<PD6);
+    //DDRD = 0b01000000;
+    //PORTD = 0b10111111;
+    //PORTD = 0xFF;
 
-    TCCR0A = TCCR0A | 0b10000011;
-    TCCR0A = TCCR0A & 0b10001111;
+    //TCNT0 = 0x00;
+    //OCR0A = 255;
 
-    TCCR0B = TCCR0B | 0b00001010;
-    TCCR0B = TCCR0B & 0b11111010;
+    //TCCR0A = TCCR0A | 0b10000011;
+    //TCCR0A = TCCR0A & 0b10001111;
 
-    //TIMSK0 = TIMSK0 | 0b00000010;
+    //TCCR0B = TCCR0B | 0b00001010;
+    //TCCR0B = TCCR0B & 0b11111010;
+
+    TCCR0A |= (1<<COM0A1) | (1<<WGM00) | (1<<WGM01);
+    TCCR0A &= ~(1<<COM0A0);
+    TCCR0B |= (1<<CS01);
+    TCCR0B &= ~((1<<FOC0A) | (1<<FOC0B) | (1<<CS00) | (1<<CS02));
+
 
     //use a 10kOhm resistor for voltage divider
-	//ADC einstellen
+    //ADC einstellen
     //select ADC0
     //no adjustment
-	ADMUX = ADMUX & 0b00010000;
+    ADMUX = ADMUX & 0b00010000;
 
-	ADCSRA |= 0b11001111;
-	ADCSRA |= 0b11011111;
+    //ADCSRA |= 0b11001111;
+    ADCSRA |= 0b11011111;
+
+    OCR0A=255;
 
     sei(); //set enable interrups: Interrups global zulassen
 
-	//set_sleep_mode(SLEEP_MODE_ADC); //ADC Sleep Modus
+    //set_sleep_mode(SLEEP_MODE_ADC); //ADC Sleep Modus
     //enter ADC noise reduction
     //SMCR = SMCR | 0b00000011;
     //SMCR = SMCR & 0b11110011;
-    set_sleep_mode(SLEEP_MODE_IDLE);
+    set_sleep_mode(SLEEP_MODE_ADC);
 
-    OCR0A = 16;
     while(1) {
         sleep_mode();
+        //PORTD ^= 0xFF;
+        //_delay_ms(10000);
     }
 
     return 0;
 
 }
 
-void ton(uint16_t freq) {
-
-	if (freq == 0){
-		TCCR0A = TCCR0A & 0b01111111;
-	}
-
-	else {
-		TCCR0A = TCCR0A | 0b10000000;
-		OCR0A = TAKT/(freq * 2 * PRESCALER) - 1;
-	}
-
-}
+//void ton(uint16_t freq) {
+//
+//    if (freq == 0){
+//        TCCR0A = TCCR0A & 0b01111111;
+//    }
+//
+//    else {
+//        TCCR0A = TCCR0A | 0b10000000;
+//        OCR0A = TAKT/(freq * 2 * PRESCALER) - 1;
+//    }
+//
+//}
 
 
 //Interrupt Service Routine für den ADC
@@ -76,7 +88,9 @@ ISR(ADC_vect) {
     //use bitwise or to add this to freq
     //=> 10bit ADC sample
     freq = freq | (ADCH << 8);
-    if(372 <= freq && freq <= 983) {
+    if(158 <= freq && freq <= 922) {
+        //OCR0A = 3.18*freq+110.2;
+        OCR0A--;
         //create a tune
         //ton((8.02*freq)-2883.31);
     } else {
